b047d9aa34425996ffb08d9f733373a73c2b4ab4
Philippe Roussille <proussille@gmail.com>
2012-03-02 16:56:18 +0100

    Adding default layout

--- libraries/View.php
+++ libraries/View.php
@@ -10,15 +10,20 @@ class View {
     private $loader;
     private $twig;
     private $template;
+    private $layout;
 
     public function __construct($view){
 
         $this->loader = new Twig_Loader_Filesystem(APP_VIEWS_PATH);
         // Attention le dossier cache doit etre en premission drwxrwxrwx
         $this->twig   = new Twig_Environment($this->loader, array(
-            'cache' => APP_PATH . '/cache',
+           // 'cache' => APP_PATH . '/cache',
         ));
-        $this->template = $this->twig->loadTemplate($view.".twig");
+        if(!is_file(APP_VIEWS_PATH . "/" . $view  .".twig")){
+            $this->template = $this->twig->loadTemplate("layout.twig");
+        }else{
+            $this->template = $this->twig->loadTemplate($view  .".twig");
+        }
 
     }
 
