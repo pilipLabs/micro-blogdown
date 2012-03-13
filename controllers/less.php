e7e18fbc2adbd3ded79d08499d6383ecfec26cd1
Philippe Roussille <proussille@gmail.com>
2012-03-13 19:02:02 +0100

    ajout less

--- controllers/less.php
+++ controllers/less.php
@@ -4,12 +4,12 @@ require_once FRAMEWORK_PATH.'/lessphp/lessc.inc.php';
 class less extends Controller{
     function index() {
         header('Content-Type:text/css; charset:utf-8;');
-        $input = FRAMEWORK_PATH.'/bootstrap/less/bootstrap.less';
+        $input = FRAMEWORK_PATH.'/my.less';
         $lc = new lessc($input);
+        $out = "";
         try {
-            $out  = $lc->parse();
-            $out .= $lc->parse('@import "reset.less";');
-            self::render('less', array('css' => $lc->parse()));
+            $out .= $lc->parse();
+            self::render('less', array('css' => $out));
         } catch (Exception $ex) {
            self::error($ex);
         }
